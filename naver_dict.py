d5fcb8b10138993b947a6d24199fa46903683cf6
gcflames5 <gcflames5@gmail.com>
2023-09-18 00:20:34 -0400

    change naver_dict.py to use new voice API

--- naver_dict.py
+++ naver_dict.py
@@ -7,31 +7,38 @@ class NaverDict():
     '''
     Utilities for the Naver dictionary
     '''
-    def __init__(self):
- 
-        self.base_url = 'https://dict.naver.com/search.dict?target=dic&ie=utf8&query_utf=&isOnlyViewEE=' # dictionary url
-        self.playlist_regex = re.compile(r'playlist=\"([^ ]*)\"') # regex of the first audio clip
  
     def fetch_audio_file(self, word, output_folder):
         # Build the rest of the url
-        word_escaped = requests.utils.quote(word)
-        dict_url = f'{self.base_url}&dicQuery={word_escaped}&query={word_escaped}'
+        headers = {
+            'authority': 'dict.naver.com',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'range': 'bytes=0-',
+            'referer': 'https://dict.naver.com/dict.search?query=%EB%AC%B8',
+            'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'audio',
+            'sec-fetch-mode': 'no-cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        }
 
-        # Fetch the dictionary page & parse playlists
-        dict_page = requests.get(dict_url)
-        playlist_matches = self.playlist_regex.findall(dict_page.text)
+        params = {
+            'service': 'dictionary',
+            'speech_fmt': 'mp3',
+            'speaker': 'mijin',
+            'text': word,
+        }
 
-        if playlist_matches == None or len(playlist_matches) < 1:
-            raise Exception(f'Saw no playlist_matches for {word}! URL: {dict_url}')
-
-        # Grab the audio and write it to a file
-        audio_page = requests.get(playlist_matches[0])
+        response = requests.get('https://dict.naver.com/api/nvoice', params=params, headers=headers)
 
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
             
         with open(os.path.join(output_folder, f'{word}.mp3'), 'wb') as f:
-            f.write(audio_page.content)
+            f.write(response.content)
 
     def bulk_fetch(self, words, output_folder):
         for word in words:
